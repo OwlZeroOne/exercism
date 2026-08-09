ce9f4518a43459e0c56d723d8667954e9ec8114d
Mateusz Pasternak <mat.pasternak06@gmail.com>
2026-08-09 16:07:08 +0100

    Complete Log Line Parser; Update README

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 |Java|![](https://geps.dev/progress/2?min=0&max=158)|
 |JavaScript|![](https://geps.dev/progress/7?min=0&max=159)|
 |Python|![](https://geps.dev/progress/12?min=0&max=146)|
-|Ruby|![](https://geps.dev/progress/3?min=0&max=121)|
+|Ruby|![](https://geps.dev/progress/4?min=0&max=121)|
 |TypeScript|![](https://geps.dev/progress/0?min=0&max=108)|
 
 ## Submitted Exercises
--- ruby/log-line-parser/log_line_parser.rb
+++ ruby/log-line-parser/log_line_parser.rb
@@ -3,15 +3,28 @@ class LogLineParser
     @line = line
   end
 
+  def line
+    return @line
+  end
+
   def message
-    raise 'Please implement the LogLineParser#message method'
+    return @line.gsub(/\[.*?\]\:/, '').strip
   end
 
   def log_level
-    raise 'Please implement the LogLineParser#log_level method'
+    line_split = @line.split(" ")
+    log = line_split[0]
+    return log[1, (log.size - 3)].downcase
   end
 
   def reformat
-    raise 'Please implement the LogLineParser#reformat method'
+    return "#{self.message} (#{self.log_level})"
   end
 end
+
+msg = "[WARNING]:  Disk almost full\r\n"
+llp = LogLineParser.new(msg)
+puts llp.line
+puts llp.message
+puts llp.log_level
+puts llp.reformat
